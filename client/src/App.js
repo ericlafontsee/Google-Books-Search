18835cd984b64a244faa0b0df9042a36a830a5ec
Eric LaFontsee <elafontsee@gmail.com>
2020-10-22 20:57:44 -0500

    added api.js in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import GoogleForm from "./components/Form";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <GoogleForm />
-    </>
+    <Router>
+        <div>
+        <Switch>
+          <Route exact path={process.env.PUBLIC_URL + "/"} component={Search} />
+          <Route exact path={process.env.PUBLIC_URL + "/books"} component={Search} />
+          <Route exact path={process.env.PUBLIC_URL + "/books/:id"} component={Saved} />
+          <Route path={process.env.PUBLIC_URL + "*"} component={Search} /> 
+        <Books />
+        </Switch>
+      </div>
+      </Router>
   );
 }
 
