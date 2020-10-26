b0c712fc3dbfe7fdb95b91f8801f110188e81ec9
Eric LaFontsee <elafontsee@gmail.com>
2020-10-26 10:56:34 -0400

    Took out fallback code in server

---
+++ client/build/precache-manifest.4d98a0426fe4074388ec93517426d321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49e921a5b0d8967e210d",
+    "url": "/static/css/main.1014a46b.chunk.css"
+  },
+  {
+    "revision": "49e921a5b0d8967e210d",
+    "url": "/static/js/main.d40d82dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c691810fc8cbbd521f3",
+    "url": "/static/css/2.96526b21.chunk.css"
+  },
+  {
+    "revision": "5c691810fc8cbbd521f3",
+    "url": "/static/js/2.1aabcd5e.chunk.js"
+  },
+  {
+    "revision": "d6db3524567cdbba78157de4a5f38747",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -15,10 +15,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
